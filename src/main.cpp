2d0843d3b6923e4213eb810cf49da93c0f9762f9
faikhadutoglu <167749575+faikhadutoglu@users.noreply.github.com>
2024-06-03 16:49:05 +0200

    datein wurden bearbeitet

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,14 @@ int main() {
     auto junior = std::make_shared<JuniorDeveloper>("Peter Parker", "Spiderman");
     auto senior = std::make_shared<SeniorDeveloper>("Diana Prince", "Wonder Woman");
 
+    try {
+        junior->load_logo_from_file("logos/spiderman.txt");
+        senior->load_logo_from_file("logos/wonderwoman.txt");
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
     developers.push_back(junior);
     developers.push_back(senior);
 
